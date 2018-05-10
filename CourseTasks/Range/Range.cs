d714cb651eccb4e46349932224cf8072c7503844
Даргеев А.А <dargeev@gmail.com>
2018-05-10 19:55:54 +0700

    Работа с замечаниями 1: - выделил мейн в отдельный класс (файл) - объединил Range и RangePlus - переделал все статисческие методы на нестатические - перестал возвращать из методов существующие объекты, переделал на создание копий

---
+++ CourseTasks/Range/Range.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Academit.DargeevAleksandr
+{
+    class Range
+    {
+        public double From
+        {
+            get;
+            set;
+        }
+
+        public double To
+        {
+            get;
+            set;
+        }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double x)
+        {
+            return (x >= From && x <= To);
+        }
+
+        public Range GetIntersection(Range interval)
+        {
+            if (From < interval.To && To > interval.From)
+            {
+                double newFrom = (From > interval.From) ? From : interval.From;
+                double newTo = (To > interval.To) ? interval.To : To;
+
+                return new Range(newFrom, newTo);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Range[] GetUnion(Range interval)
+        {
+            if (From > interval.To || To < interval.From)
+            {
+                return new Range[] { new Range(From, To), new Range(interval.From, interval.To) };
+            }
+            else
+            {
+                double newFrom = (From > interval.From) ? interval.From : From;
+                double newTo = (To > interval.To) ? To : interval.To;
+
+                return new Range[] { new Range(newFrom, newTo) };
+            }
+        }
+
+        public Range[] GetDifference(Range interval)
+        {
+            if (From < interval.To && To > interval.From)
+            {
+                if (From > interval.From && To > interval.To)
+                {
+                    return new Range[] { new Range(interval.To, To) };
+                }
+                else if (From < interval.From && To < interval.To)
+                {
+                    return new Range[] { new Range(From, interval.From) };
+                }
+                else
+                {
+                    return new Range[] { new Range(From, interval.From), new Range(interval.To, To) };
+                }
+            }
+            else
+            {
+                return new Range[] { new Range(From, To) };
+            }
+        }
+    }
+}
--- CourseTasks/Range/RangeHomework.cs
+++ CourseTasks/Range/RangeHomework.cs
@@ -2,73 +2,64 @@
 
 namespace Academit.DargeevAleksandr
 {
-    class Range
-    {
-        public double From
-        {
-            get;
-            set;
-        }
-
-        public double To
-        {
-            get;
-            set;
-        }
-
-        public Range(double from, double to)
-        {
-            From = from;
-            To = to;
-        }
-
-        public double GetLength()
-        {
-            return To - From;
-        }
-
-        public bool IsInside(double x)
-        {
-            double epsilon = 1.0e-10;
-
-            return (x - From >= -epsilon && x - To <= epsilon);
-        }
-    }
-
     class RangeHomework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите начало и конец интервала:");
+            Console.WriteLine("Введите начало и конец первого интервала:");
 
             Console.Write("начало интервала = ");
-            double from = Convert.ToDouble(Console.ReadLine());
+            double from1 = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("конец интервала = ");
-            double to = Convert.ToDouble(Console.ReadLine());
+            double to1 = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine();
 
-            Range interval = new Range(from, to);
+            Console.WriteLine("Введите начало и конец второго интервала:");
 
-            Console.WriteLine($"Длина введенного интервала = {interval.GetLength():f2}");
+            Console.Write("начало интервала = ");
+            double from2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("конец интервала = ");
+            double to2 = Convert.ToDouble(Console.ReadLine());
+
             Console.WriteLine();
 
-            Console.Write("Введите число для проверки на вхождение в заданный интервал: ");
-            double x = Convert.ToDouble(Console.ReadLine());
+            Range interval1 = new Range(from1, to1);
+            Range interval2 = new Range(from2, to2);
 
-            Console.WriteLine();
+            Range intersection = interval1.GetIntersection(interval2);
 
-            if (interval.IsInside(x))
+            if (intersection != null)
             {
-                Console.Write("Введенное число входит в интервал.");
+                Console.WriteLine("Пересечение интервалов:");
+                Console.WriteLine("[{0}, {1}]", intersection.From, intersection.To);
             }
             else
             {
-                Console.Write("Введенное число не входит в интервал.");
+                Console.WriteLine("Пересечение отсутствует.");
             }
 
+            Range[] intervalsSum = interval1.GetUnion(interval2);
+
             Console.WriteLine();
+            Console.WriteLine("Объединение интервалов:");
+
+            foreach (Range a in intervalsSum)
+            {
+                Console.WriteLine("[{0}, {1}]", a.From, a.To);
+            }
+
+            Range[] intervalsDifference = interval1.GetDifference(interval2);
+
+            Console.WriteLine();
+            Console.WriteLine("Разность интервалов:");
+
+            foreach (Range a in intervalsDifference)
+            {
+                Console.WriteLine("[{0}, {1}]", a.From, a.To);
+            }
         }
     }
 }
