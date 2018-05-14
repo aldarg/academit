12efb6eeb05970253116d876f3e80d0277d6f4ee
Даргеев А.А <dargeev@gmail.com>
2018-05-14 09:43:54 +0700

    Векторы - добавил проверку в сеттер на массив с длиной 0

--- CourseTasks/Matrix/Matrix.cs
+++ CourseTasks/Matrix/Matrix.cs
@@ -1,10 +1,351 @@
 ﻿using System;
-using 
+using System.Text;
 
 namespace Academits.DargeevAleksandr
 {
-    class Matrix
+    public class Matrix
     {
+        public Vector[] Rows
+        {
+            private get;
+            set;
+        }
 
+        public Matrix(int n, int m)
+        {
+            if (n <= 0 || m <= 0 || (n == 1 && m == 1))
+            {
+                throw new ArgumentException("Matrix: неверные размеры матрицы.");
+            }
+
+            Rows = new Vector[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Rows[i] = new Vector(m);
+            }
+        }
+
+        public Matrix(Matrix original)
+        {
+            Rows = new Vector[original.GetSize(0)];
+
+            Array.Copy(original.Rows, Rows, original.GetSize(0));
+        }
+
+        public Matrix(double[,] a)
+        {
+            if (a.Length <= 1)
+            {
+                throw new ArgumentException("Размерность параметра <массив> должна быть больше 1.");
+            }
+
+            Rows = new Vector[a.GetLength(0)];
+
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                double[] temp = new double[a.GetLength(1)];
+
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    temp[j] = a[i, j];
+                }
+
+                Rows[i] = new Vector(temp);
+            }
+        }
+
+        public Matrix(Vector[] vectors)
+        {
+            if (vectors.Length <= 1 && vectors[0].Size <= 1)
+            {
+                throw new ArgumentException("");
+            }
+
+            Rows = new Vector[vectors.Length];
+
+            int m = 0;
+
+            foreach (Vector v in vectors)
+            {
+                if (m < v.Size)
+                {
+                    m = v.Size;
+                }
+            }
+
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                if (vectors[i].Size == m)
+                {
+                    Rows[i] = vectors[i];
+                }
+                else
+                {
+                    Rows[i] = Vector.GetSum(vectors[i], new Vector(m));
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.Append("{ ");
+
+            for (int i = 0; i < GetSize(0) - 1; i++)
+            {
+                result.Append(Rows[i]).Append(", ");
+            }
+
+            result.Append(Rows[GetSize(0) - 1]);
+            result.Append(" }");
+
+            return result.ToString();
+        }
+
+        public int GetSize(int dimension)
+        {
+            if (dimension == 0)
+            {
+                return Rows.Length;
+            }
+            else if (dimension == 1)
+            {
+                return Rows[0].Size;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Неверный параметр размерности.");
+            }
+        }
+
+        public Vector GetRow(int index)
+        {
+            if (index < 0 || index >= GetSize(0))
+            {
+                throw new ArgumentException("GetRow: Некорректный индекс.");
+            }
+
+            return Rows[index];
+        }
+
+        public void SetRow(Vector row, int index)
+        {
+            if (index < 0 || index >= GetSize(0))
+            {
+                throw new ArgumentException("SetRow: Некорректный индекс.");
+            }
+
+            if (row.Size != GetSize(1))
+            {
+                throw new ArgumentException("SetRow: размерность строки не подходит под размерность матрицы.");
+            }
+
+            Rows[index] = new Vector(row);
+        }
+
+        public void SetRow(double[] a, int index)
+        {
+            if (index < 0 || index >= GetSize(0))
+            {
+                throw new ArgumentException("SetRow: Некорректный индекс.");
+            }
+
+            if (a.Length != GetSize(1))
+            {
+                throw new ArgumentException("SetRow: размерность строки не подходит под размерность матрицы.");
+            }
+
+            Rows[index] = new Vector(a);
+        }
+
+        public Vector GetColumn(int index)
+        {
+            if (index < 0 || index >= GetSize(1))
+            {
+                throw new ArgumentException("GetColumn: Некорректный индекс.");
+            }
+
+            double[] temp = new double[GetSize(0)];
+
+            for (int i = 0; i < GetSize(0); i++)
+            {
+                temp[i] = Rows[i].GetByIndex(index);
+            }
+
+            return new Vector(temp);
+        }
+
+        public void Transpose()
+        {
+            Vector[] temp = new Vector[GetSize(1)];
+
+            for (int i = 0; i < GetSize(1); i++)
+            {
+                temp[i] = GetColumn(i);
+            }
+
+            Rows = temp;
+        }
+
+        public void Multiply(double scalar)
+        {
+            for (int i = 0; i < GetSize(0); i++)
+            {
+                Rows[i].Multiply(scalar);
+            }
+        }
+
+        private static Matrix GetMinorMatrix(Matrix matrix, int i, int j)
+        {
+            int dimension = matrix.GetSize(0) - 1;
+            Matrix minorMatrix = new Matrix(dimension, dimension);
+
+            int xOffset = 0;
+            int yOffset = 0;
+
+            for (int k = 0; k < dimension; k++)
+            {
+                if (k == i)
+                {
+                    yOffset = 1;
+                }
+
+                double[] temp = new double[dimension];
+
+                for (int m = 0; m < dimension; m++)
+                {
+                    if (m == j)
+                    {
+                        xOffset = 1;
+                    }
+
+                    temp[m] = matrix.Rows[k + yOffset].GetByIndex(m + xOffset);
+                }
+
+                minorMatrix.SetRow(temp, k);
+
+                xOffset = 0;
+            }
+
+            return minorMatrix;
+        }
+
+        private static double GetDeterminantStatic(Matrix matrix)
+        {
+            if (matrix.GetSize(0) == 1)
+            {
+                return matrix.GetRow(0).GetByIndex(0);
+            }
+
+            if (matrix.GetSize(0) == 2)
+            {
+                return matrix.GetRow(0).GetByIndex(0) * matrix.GetRow(1).GetByIndex(1) - matrix.GetRow(0).GetByIndex(1) * matrix.GetRow(1).GetByIndex(0);
+            }
+
+            double determinant = 0;
+
+            for (int i = 0; i < matrix.GetSize(0); i++)
+            {
+                determinant += Math.Pow(-1, i) * matrix.GetRow(0).GetByIndex(i) * GetDeterminantStatic(GetMinorMatrix(matrix, 0, i));
+            }
+
+            return determinant;
+        }
+
+        public double GetDeterminant()
+        {
+            if (GetSize(0) != GetSize(1))
+            {
+                throw new Exception("Неверная размерность матрицы для вычисления определителя.");
+            }
+
+            Matrix temp = new Matrix(Rows);
+
+            return GetDeterminantStatic(temp);
+        }
+
+        public void Multiply(Vector vector)
+        {
+            if (GetSize(1) != vector.Size)
+            {
+                throw new ArgumentException("Неверная размерность вектора.");
+            }
+
+            Matrix result = new Matrix(GetSize(0), 1);
+
+            for (int i = 0; i < GetSize(0); i++)
+            {
+                double[] temp = new double[1];
+                temp[0] = Vector.GetScalarProduct(Rows[i], vector);
+
+                result.Rows[i] = new Vector(temp);
+            }
+
+            Rows = result.Rows;
+        }
+
+        public void Add(Matrix matrix)
+        {
+            if (GetSize(0) != matrix.GetSize(0) || GetSize(1) != matrix.GetSize(1))
+            {
+                throw new ArgumentException("Операции сложения и вычитания матриц возможны только для матриц одного размера.");
+            }
+
+            for (int i = 0; i < GetSize(0); i++)
+            {
+                Rows[i].Add(matrix.Rows[i]);
+            }
+        }
+
+        public void Subtract(Matrix matrix)
+        {
+            Matrix temp = new Matrix(matrix);
+
+            temp.Multiply(-1);
+            Add(temp);
+        }
+
+        public static Matrix GetSum(Matrix matrix1, Matrix matrix2)
+        {
+            Matrix result = new Matrix(matrix1);
+
+            result.Add(matrix2);
+
+            return result;
+        }
+
+        public static Matrix GetDifference(Matrix matrix1, Matrix matrix2)
+        {
+            Matrix result = new Matrix(matrix1);
+
+            result.Subtract(matrix2);
+
+            return result;
+        }
+
+        public static Matrix MultiplyMatrixes(Matrix matrix1, Matrix matrix2)
+        {
+            if (matrix1.GetSize(1) != matrix2.GetSize(0))
+            {
+                throw new ArgumentException("Умножение матриц невозможно - некорректные размеры.");
+            }
+
+            Matrix result = new Matrix(matrix2.GetSize(1), matrix1.GetSize(0));
+
+            for (int i = 0; i < matrix1.GetSize(0); i++)
+            {
+                Matrix temp = new Matrix(matrix1);
+
+                temp.Multiply(matrix2.GetColumn(i));
+                temp.Transpose();
+                result.SetRow(temp.Rows[0], i);
+            }
+
+            result.Transpose();
+
+            return result;
+        }
     }
 }
--- CourseTasks/Matrix/MatrixHomework.cs
+++ CourseTasks/Matrix/MatrixHomework.cs
@@ -1,15 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Matrix
+namespace Academits.DargeevAleksandr
 {
     class MatrixHomework
     {
         static void Main(string[] args)
         {
+            Vector vector1 = new Vector(new double[] { 1, 2, 3, 4 });
+            Vector vector2 = new Vector(new double[] { 7, 8, 5, 4, 5, 6 });
+            double[,] a =
+            {
+                {1, 2, 3, 4 },
+                {6, 7, 8, 9 },
+                {4, 5, 10, 20 }
+            };
+
+            Vector[] vectors = new Vector[] { vector1, vector2 };
+
+            Matrix test1 = new Matrix(vectors);
+            Matrix test2 = new Matrix(test1);
+            Matrix test3 = new Matrix(3, 5);
+            Matrix test4 = new Matrix(a);
+
+
+            Console.WriteLine(test1);
+            Console.WriteLine(test2);
+            Console.WriteLine(test3);
+            Console.WriteLine(test4);
+
+            Console.WriteLine(test4.GetSize(0));
+            Console.WriteLine(test4.GetSize(1));
+
+            Console.WriteLine(test4.GetRow(2));
+            test4.SetRow(vector1, 2);
+            Console.WriteLine(test4);
+            test4.SetRow(new double[] { 20, 30, 40, 50 }, 2);
+            Console.WriteLine(test4);
+
+            Console.WriteLine(test4.GetColumn(2));
+
+            Console.WriteLine(test4);
+            test4.Transpose();
+            Console.WriteLine(test4);
+
+            test4.Multiply(3);
+            Console.WriteLine(test4);
+
+            double[,] b =
+            {
+                { 1, 2, 3, 6 },
+                { 6, 7, 8, 6 },
+                { 4, 5, 9, 6 },
+                { 24, 25, 29, 26 }
+            };
+
+            Matrix test5 = new Matrix(b);
+            Console.WriteLine(test5);
+            double determinant = test5.GetDeterminant();
+            Console.WriteLine(determinant);
+            Console.WriteLine(test5);
+
+            double[,] c =
+            {
+                {4, 5 },
+                {6, 7 },
+                {8, 9 }
+            };
+
+            double[,] d =
+            {
+                {1, 2, 3 },
+                {4, 5, 6 }
+            };
+
+            Matrix test6 = new Matrix(c);
+            Matrix test7 = new Matrix(d);
+
+            Console.WriteLine(Matrix.MultiplyMatrixes(test6, test7));
+
+            double[,] e = { { 1 } };
+
+            Matrix test8 = new Matrix(e);
+            Console.WriteLine(test8);
         }
     }
 }
