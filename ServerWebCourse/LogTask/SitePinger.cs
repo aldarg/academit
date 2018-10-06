9c472db93ee24cfae9781839003daf279393446d
Даргеев А.А <dargeev@gmail.com>
2018-10-06 22:47:58 +0700

    Лог и юнит-тесты: на ревью

---
+++ ServerWebCourse/LogTask/SitePinger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using NLog;
+using System.Net.NetworkInformation;
+
+namespace LogTask
+{
+    public class SitePinger
+    {
+        public static void Main(string[] args)
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+            var pinger = new Ping();
+            const string serverName = "glavnoehvost.ru";
+            const int pingsCount = 100;
+            var i = 0;
+
+            void Start()
+            {
+                ++i;
+                while (i <= pingsCount)
+                {
+                    try
+                    {
+                        var reply = pinger.Send(serverName);
+                        if (reply.Status != IPStatus.Success)
+                        {
+                            logger.Warn("Сервер недоступен.");
+                        }
+                        else
+                        {
+                            logger.Info($"Latency: {reply.RoundtripTime}");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        logger.Error(e, "Ошибка");
+                    }
+
+                    Thread.Sleep(3000);
+                }
+            }
+
+            var pingJob = new Thread(Start);
+            pingJob.Start();
+        }
+    }
+}
